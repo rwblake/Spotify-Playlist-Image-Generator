15e386bf07d595402f6dfc67309aa3310515df86
Robert Blake <rxb256@student.bham.ac.uk>
2024-02-01 20:06:03 +0000

    added mean colour average

--- average_color.py
+++ average_color.py
@@ -1,15 +1,22 @@
 
 
-from imagedominantcolor import DominantColor
-
-
 def get_dominant_colors(image_paths):
+	from imagedominantcolor import DominantColor
 	return [(path, DominantColor(path).rgb) for path in image_paths]
+
+
+def get_mean_colors(image_paths):
+	import cv2
+	import numpy
+	ret = []
+	for path in image_paths:
+		ret.append((path, list(numpy.average(numpy.average(cv2.imread(path), axis=0), axis=0))[::-1]))
+	return ret
 
 
 def get_average_colors(image_paths):
 	# averages = [(path, average)]
-	return get_dominant_colors(image_paths)
+	return get_mean_colors(image_paths)
 
 
 def main():
