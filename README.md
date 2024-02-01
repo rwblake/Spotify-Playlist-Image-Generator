01062e66d65cf94ae6593af5059519bb3689b71a
Robert Blake <rxb256@student.bham.ac.uk>
2024-02-01 14:55:20 +0000

    create script to deposit images into folder

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Spotify Playlist Image Generator
 ## Generate an aesthetic mosaic playlist image from album art in a spotify playlist.
 Input a spotify playlist URL, and the final image name.
+
+## Setup
+The spotipy "[Client Credentials Flow](https://spotipy.readthedocs.io/en/2.22.1/#client-credentials-flow)" is used. You will need to set environment variables for your own client ID and client secret, before the API will allow access.
---
+++ pull_images.py
@@ -0,0 +1,55 @@
+
+
+import os
+import requests
+import shutil
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+def pull_images(playlist_URL):
+	# authenticates request
+	auth_manager = SpotifyClientCredentials()
+	sp = spotipy.Spotify(auth_manager=auth_manager)
+
+	# assume that playlist_URL is valid
+	playlist = sp.playlist(playlist_URL)
+
+	tracks = [{"name":     item["track"]["name"],
+	           "id":       item["track"]["id"],
+	           "image_URL":item["track"]["album"]["images"][0]["url"]
+	          }
+	          for item in playlist["tracks"]["items"]
+	         ]
+
+	os.mkdir(playlist["name"])  # to store images
+	os.chdir(playlist["name"])
+	os.mkdir("images")
+	os.chdir("images")
+
+	for track in tracks:
+		res = requests.get(track["image_URL"], stream = True)
+		if res.status_code != 200:
+			print(f"Image for \'{track['name']}\' couldn't be retrieved.")
+			continue
+
+		with open(f"{track['name']}.png", "wb") as f:
+			shutil.copyfileobj(res.raw, f)
+
+	os.chdir("..")
+	# end up in the new playlist directory
+
+
+
+def main():
+	import sys
+	if len(sys.argv) != 2:
+		sys.stderr.write(f"One argument expected, {sys.argv.length()-1} provided.")
+		return
+	pull_images(sys.argv[1])
+
+
+
+if __name__ == "__main__":
+	main()
+
