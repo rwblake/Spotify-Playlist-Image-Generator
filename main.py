691f5e8640332525db3a3a76b0876482b4224a6c
Robert Blake <rxb256@student.bham.ac.uk>
2024-02-03 15:14:52 +0000

    changed to argparse cli argument parsing

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
 import os
-import sys
 import math
 from PIL import Image
 import imagehash
+import argparse
 
 
 import ordering
@@ -24,15 +24,27 @@ def filter_duplicates(paths):
 	return ret
 
 
-def main():
-	if len(sys.argv) != 5:
-		print(f"Usage: {sys.argv[0]} playlist_url reference width duplicates")
-		return
+WIDTH_UNSPECIFIED = 0
+MAXIMUM_WIDTH = -1
 
-	playlist_name = pull_images.pull_images(sys.argv[1])
-	images_wide = int(sys.argv[3])  # how many albums wide the result will be
-	reference = sys.argv[2]
-	duplicates = sys.argv[4] != "0"
+
+def get_arguments():
+	parser = argparse.ArgumentParser(description="Create a mosaic playlist cover from a reference image.")
+	parser.add_argument('playlist_URL', type=str,
+		                help="Spotify playlist URL. Starts with \'https://open.spotify.com/playlist/\'.")
+	parser.add_argument('reference_image', type=str,
+		                help="Path to reference image. Must be square, and NOT transparent.")
+	parser.add_argument('-w', '--width', nargs='?', default=WIDTH_UNSPECIFIED, const=MAXIMUM_WIDTH, type=int,
+		                help="Width of mosaic in images. Default is automatic. No value picks maximum possible width.")
+	parser.add_argument('-d', '--duplicates', action='store_true',
+		                help="Allow duplicate images in mosaic.")
+	return parser.parse_args()
+
+
+def main():
+	args = get_arguments()
+
+	playlist_name = pull_images.pull_images(args.playlist_URL)
 
 	os.chdir(playlist_name)
 	os.chdir("images")
@@ -42,19 +54,27 @@ def main():
 
 	averages = average_color.get_average_colors(image_paths)
 	print(len(averages))
+
+	max_width = math.floor(math.sqrt(len(image_paths)))
+	if args.width == MAXIMUM_WIDTH:
+		args.width = max_width
+
+	if not args.duplicates and args.width != WIDTH_UNSPECIFIED and len(image_paths) < args.width**2:
+		raise Exception(f"Not enough images to create mosaic of width {args.width}. Maximum width is {max_width} for {len(image_paths)} images.")
 	
-	if images_wide == 0:
-		images_wide = math.floor(math.sqrt(len(image_paths)))
-		orders = [ordering.ordering("../../"+reference, i, averages) for i in range(images_wide, 1, -1)]
-		for order in orders:
-			print(len(order[0]), order[1])
+	if args.width == WIDTH_UNSPECIFIED:
+		orders = []
+		for i in range(max_width, 1, -1):
+			order = ordering.ordering("../../"+args.reference_image, i, averages, args.duplicates)
+			orders.append(order)
+			print(f"Width: {i:03}\tError: {order[1]}")
 		order = min(orders, key=lambda x:x[1])[0]
 	else:
-		order = ordering.ordering("../../"+reference, images_wide, averages, duplicates)[0]
+		order = ordering.ordering("../../"+args.reference_image, args.width, averages, args.duplicates)[0]
 	print(len(order))
-	images_wide = math.floor(math.sqrt(len(order)))
 
-	image = generate_mosaic.generate_mosaic(1600, images_wide, order)
+	args.width = math.floor(math.sqrt(len(order)))
+	image = generate_mosaic.generate_mosaic(1600, args.width, order)
 	image.show()
 
 
