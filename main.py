d5c6783cba78d0325f1c8a5a713c49427c59fcba
Robert Blake <rxb256@student.bham.ac.uk>
2024-02-03 14:04:20 +0000

    removes duplicate picutes with imagehash

--- main.py
+++ main.py
@@ -3,12 +3,25 @@ import os
 import sys
 import math
 from PIL import Image
+import imagehash
 
 
 import ordering
 import average_color
 import generate_mosaic
 import pull_images
+
+
+def filter_duplicates(paths):
+	hashes = []
+	ret = []
+	for path in paths:
+		image_hash = imagehash.average_hash(Image.open(path))
+		if image_hash in hashes:
+			continue
+		hashes.append(image_hash)
+		ret.append(path)
+	return ret
 
 
 def main():
@@ -23,14 +36,22 @@ def main():
 	os.chdir(playlist_name)
 	os.chdir("images")
 
-	if images_wide == 0:
-		images_wide = math.floor(math.sqrt(len(os.listdir('.'))))
 	image_paths = os.listdir(".")
+	image_paths = filter_duplicates(image_paths)
 
 	averages = average_color.get_average_colors(image_paths)
 	print(len(averages))
-	order = ordering.ordering("../../"+reference, images_wide, averages)
+	
+	if images_wide == 0:
+		images_wide = math.floor(math.sqrt(len(image_paths)))
+		orders = [ordering.ordering("../../"+reference, i, averages) for i in range(images_wide, 1, -1)]
+		for order in orders:
+			print(len(order[0]), order[1])
+		order = min(orders, key=lambda x:x[1])[0]
+	else:
+		order = ordering.ordering("../../"+reference, images_wide, averages)[0]
 	print(len(order))
+	images_wide = math.floor(math.sqrt(len(order)))
 
 	image = generate_mosaic.generate_mosaic(1600, images_wide, order)
 	image.show()
